2be2b4e7f16d8a7ad085b5b78113d4651bf825dd
Aniket Jha <aniketjha@Anikets-Laptop.local>
2024-09-17 20:11:11 +0530

    Gym Removed

--- src/Components/Details.js
+++ src/Components/Details.js
@@ -54,10 +54,7 @@ const Details = ({propertyDetail}) => {
                 <img className='lg:w-[50px] w-[30px] mt-[-18px]' src={Elevator} />
                 <p className='font-[NeueRegular]  text-[0.8rem]  lg:text-[1rem]' >Elevator</p>
             </div>
-            <div className='flex mt-6 items-center justify-start gap-4' >
-                <img className='lg:w-[50px] w-[30px] mt-[-25px]' src={Gym} />
-                <p className='font-[NeueRegular]  text-[0.8rem]  lg:text-[1rem]' >Gym</p>
-            </div>
+            
             <div className='flex mt-6 items-center justify-start gap-4' >
                 <img className='lg:w-[50px] w-[30px] mt-[-18px]' src={Food} />
                 <p className='font-[NeueRegular]  text-[0.8rem]  lg:text-[1rem]' >Meals</p>
